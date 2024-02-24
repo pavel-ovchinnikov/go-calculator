caf8825bb6eccc86421290fb13b95e55b9396c6a
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2024-02-24 21:59:46 +0300

    update handler

--- internal/handler/sum_handler.go
+++ internal/handler/sum_handler.go
@@ -1,10 +1,29 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
 
 type SumHandler struct{}
 
+type SumRequest struct {
+	A int
+	B int
+}
+
 func (h *SumHandler) Handler(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Request: %s", r.Body)
+
+	req := SumRequest{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		slog.Error("%s", err)
+		return
+	}
+	slog.Info("Request: %+v", req)
+
 	w.Write([]byte("sum handler"))
 }
 
